a1bd7ff8bae074b4110bfdc9c643b74b7a8df3d9
jsrosman <jsrosman@gmail.com>
2023-12-18 16:08:47 +0100

    Create TypeModelEditor
    to edit (multilevel) .type files

---
+++ app/scripts/ide/modeleditors/type/typemodeleditor.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+class TypeModelEditor extends ModelEditor {
+    /**
+     * This editor handles type models; only validates the xml
+     * @param {IDE} ide 
+     * @param {TypeFile} file The ServerFile to be loaded. E.g. 'customer.type', 'order.type'
+     */
+    constructor(ide, file) {
+        super(ide, file);
+        this.typeEditor = new TypeEditor(this.ide, file, this.htmlContainer);
+    }
+
+    get label() {
+        return 'Edit Type - ' + this.fileName;
+    }
+
+    onEscapeKey(e) {
+        if (e.target instanceof HTMLInputElement || e.target instanceof HTMLTextAreaElement) {
+            return;
+        }
+        this.close();
+    }
+
+    loadModel() {
+        this.typeEditor.loadModel();
+    }
+}
