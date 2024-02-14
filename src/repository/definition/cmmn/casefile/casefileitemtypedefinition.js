80b0b6f2511ffae939a82ea0100de6e3968314eb
jsrosman <jsrosman@gmail.com>
2024-02-14 11:55:28 +0100

    Support Type Definitions in CaseFileDefinition
    Adding a typeRef property to the case file xml tag, and stop exporting CFI children if it has a value.
    Put a boolean to indicate whether old or new model is used.
    Also wrap and expose TypeDefinitions as normal case file children through CaseFileItemTypeDefinition

---
+++ src/repository/definition/cmmn/casefile/casefileitemtypedefinition.js
@@ -0,0 +1,71 @@
+import SchemaPropertyDefinition from "@repository/definition/type/schemapropertydefinition";
+import CaseDefinition from "../casedefinition";
+import CaseFileItemDef from "./casefileitemdef";
+
+export default class CaseFileItemTypeDefinition extends CaseFileItemDef {
+    /**
+     * 
+     * @param {CaseDefinition} parent 
+     * @param {String} id
+     */
+    static createEmptyDefinition(parent, id = undefined) {
+        const definition = parent.createDefinition(CaseFileItemDef, id, '');
+        definition.isEmpty = true;
+        return definition;
+    }
+
+    /**
+     * 
+     * @param {CaseDefinition} caseDefinition 
+     * @param {*} parent 
+     * @param {SchemaPropertyDefinition} propertyDefinition 
+     */
+    constructor(caseDefinition, parent, propertyDefinition) {
+        super(undefined, caseDefinition, parent);
+        this.property = propertyDefinition;
+        this.name = propertyDefinition.name;
+        this.id = this.getPath();
+        this.multiplicity = propertyDefinition.multiplicity;
+
+        const childProperties = this.property.schema ? this.property.schema.properties : this.property.subType ? this.property.subType.schema.properties : [];
+        childProperties.filter(p => p.isComplexType).forEach(child => this.children.push(new CaseFileItemTypeDefinition(this.caseDefinition, this, child)));
+
+        this.isEmpty = false;
+    }
+
+    referencesElement(element) {
+        return element === this.property;
+    }
+
+    getPath() {
+        const parentPaths = [];
+        let ancestor = this.parent;
+        while (ancestor && ancestor instanceof CaseFileItemTypeDefinition) {
+            parentPaths.push(ancestor.property.name);
+            ancestor = ancestor.parent;
+        }
+        const parent = parentPaths.filter(p => p !== '').reverse().join('/');
+        return parent.length > 0 ? parent + '/' + this.property.name : this.property.name;
+    }
+
+    /**
+     * Returns the default transition for this type of plan item.
+     * @returns {String}
+     */
+    get defaultTransition() {
+        return 'create';
+    }
+
+    /**
+     * Returns all descending case file items including this one, recursively.
+     */
+    getDescendants() {
+        const descendants = [this];
+        this.children.forEach(child => child.getDescendants().forEach(c => descendants.push(c)));
+        return descendants;
+    }
+
+    createExportNode() {
+        return;
+    }
+}
