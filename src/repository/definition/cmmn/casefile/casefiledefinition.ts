b3cf064bc49bf2fc2a6f5070aba32abdb3f51b41
jsrosman <jsrosman@gmail.com>
2024-02-14 11:55:28 +0100

    Support Type Definitions in CaseFileDefinition
    Adding a typeRef property to the case file xml tag, and stop exporting CFI children if it has a value.
    Put a boolean to indicate whether old or new model is used.
    Also wrap and expose TypeDefinitions as normal case file children through CaseFileItemTypeDefinition

--- src/repository/definition/cmmn/casefile/casefiledefinition.ts
+++ src/repository/definition/cmmn/casefile/casefiledefinition.ts
@@ -1,15 +1,37 @@
 import CMMNElementDefinition from "@repository/definition/cmmnelementdefinition";
 import CaseDefinition from "../casedefinition";
+import TypeDefinition from "@repository/definition/type/typedefinition";
 import CaseFileItemDef, { CaseFileItemCollection } from "./casefileitemdef";
+import CaseFileItemTypeDefinition from "./casefileitemtypedefinition";
 
 export default class CaseFileDefinition extends CaseFileItemCollection {
+    isOldStyle: boolean;
+    typeRef: string;
+    type?: TypeDefinition;
     constructor(importNode: Element, caseDefinition: CaseDefinition, parent: CMMNElementDefinition) {
         super(importNode, caseDefinition, parent);
         this.parseElements('caseFileItem', CaseFileItemDef, this.children);
+        this.isOldStyle = this.children.length > 0; // If we have found the <caseFileItem> tag, then it is an old style model.
+        this.typeRef = this.parseAttribute('typeRef');
+    }
+
+    hasExternalReferences() {
+        return this.typeRef !== '';
+    }
+
+    async loadExternalReferences() {
+        return this.resolveExternalDefinition<TypeDefinition>(this.typeRef).then(definition => {
+            if (definition) {
+                this.type = definition;
+                this.type.schema?.properties.forEach(property => this.children.push(new CaseFileItemTypeDefinition(this.caseDefinition, this, property)));
+            }
+        });
     }
 
     createExportNode(parentNode: Element) {
-        super.createExportNode(parentNode, 'caseFileModel', 'children');
+        // Only export children if typeRef is empty
+        const propertiesToExport = ['typeRef', this.typeRef ? '' : 'children'];
+        super.createExportNode(parentNode, 'caseFileModel', ...propertiesToExport);
     }
 
     /**
---
+++ src/repository/definition/cmmn/casefile/casefileitemtypedefinition.ts
@@ -0,0 +1,68 @@
+import SchemaPropertyDefinition from "@repository/definition/type/schemapropertydefinition";
+import CaseDefinition from "../casedefinition";
+import CaseFileItemDef from "./casefileitemdef";
+import ElementDefinition from "@repository/definition/elementdefinition";
+import ModelDefinition from "@repository/definition/modeldefinition";
+
+export default class CaseFileItemTypeDefinition extends CaseFileItemDef {
+    property: SchemaPropertyDefinition;
+    /**
+     * 
+     * @param {CaseDefinition} parent 
+     * @param {String} id
+     */
+    static createEmptyDefinition(parent: CaseDefinition, id = undefined) {
+        const definition: CaseFileItemDef = parent.createDefinition(CaseFileItemDef, id, '');
+        definition.isEmpty = true;
+        return definition;
+    }
+
+    /**
+     * 
+     * @param {CaseDefinition} caseDefinition 
+     * @param {*} parent 
+     * @param {SchemaPropertyDefinition} propertyDefinition 
+     */
+    constructor(caseDefinition: CaseDefinition, parent: any, propertyDefinition: SchemaPropertyDefinition) {
+        super(caseDefinition.importNode.ownerDocument.createElement('will-not-be-exported'), caseDefinition, parent);
+        this.property = propertyDefinition;
+        this.name = propertyDefinition.name;
+        this.id = this.getPath();
+        this.multiplicity = propertyDefinition.multiplicity;
+
+        const childProperties: SchemaPropertyDefinition[] = this.property.schema ? this.property.schema.properties : this.property.subType ? this.property.subType?.schema?.properties || [] : [];
+        childProperties.filter(p => p.isComplexType).forEach(child => this.children.push(new CaseFileItemTypeDefinition(this.caseDefinition, this, child)));
+
+        this.isEmpty = false;
+    }
+
+    referencesElement<X extends ModelDefinition>(element: ElementDefinition<X>) {
+        return element.id === this.property.id;
+    }
+
+    getPath() {
+        const parentPaths = [];
+        let ancestor = this.parent;
+        while (ancestor && ancestor instanceof CaseFileItemTypeDefinition) {
+            parentPaths.push(ancestor.property.name);
+            ancestor = ancestor.parent;
+        }
+        const parent = parentPaths.filter(p => p !== '').reverse().join('/');
+        return parent.length > 0 ? parent + '/' + this.property.name : this.property.name;
+    }
+
+
+    /**
+     * Returns all descending case file items including this one, recursively.
+     */
+    getDescendants() {
+        const descendants: CaseFileItemDef[] = [this];
+        this.children.forEach(child => child.getDescendants().forEach((c: CaseFileItemDef) => descendants.push(c)));
+        return descendants;
+    }
+
+    createExportNode() {
+        // We do not need to create any export XML, as the parent CaseFile will set a 'typeRef' attribute that includes us.
+        return;
+    }
+}
