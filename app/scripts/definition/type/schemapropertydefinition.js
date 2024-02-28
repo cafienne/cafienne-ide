ffe47f297ba749a4c384065642e91f8fce838255
jsrosman <jsrosman@gmail.com>
2024-02-28 14:44:23 +0100

    Fix in XML to to-JSON-Schema  mapping voor array items

--- app/scripts/definition/type/schemapropertydefinition.js
+++ app/scripts/definition/type/schemapropertydefinition.js
@@ -82,6 +82,7 @@ class SchemaPropertyDefinition extends ReferableElementDefinition {
         const jsonProperty = {};
         properties[this.name] = jsonProperty;
         jsonProperty.$id = this.id;
+        jsonProperty.title = this.name; // Default label is the name of the property
         /** @type {object} */
         let property =  jsonProperty;
         if (this.type === 'object') {
@@ -104,28 +105,49 @@ class SchemaPropertyDefinition extends ReferableElementDefinition {
             break;
         case 'ZeroOrMore':
             // Array with optional items
-            jsonProperty.minItems = 0;
-            jsonProperty.type = 'array';
             // Array items will have the type of the property
-            property = jsonProperty.items = {};
+            this.setJSONArrayItemType(jsonProperty, 0);
             break;
         case 'OneOrMore':
             // Array with at least on required item
             required.push(this.name);
-            jsonProperty.minItems = 1;
-            jsonProperty.type = 'array';
             // Array items will have the type of the property
-            property = jsonProperty.items = {};
+            this.setJSONArrayItemType(jsonProperty, 1);
             break;
         case 'Unspecified':
             // Array with unspecified number of items
-            jsonProperty.type = 'array';
             // Array items will have the type of the property
-            property = jsonProperty.items = {};
+            this.setJSONArrayItemType(jsonProperty);
             break;
         case 'Unknown':
             break;
         }
         return jsonProperty;
+    }
+
+    setJSONArrayItemType(property, minItems=undefined, maxItems=undefined) {
+        if (minItems) {
+            property.minItems = minItems;
+        }
+        if (maxItems) {
+            property.maxItems = maxItems;
+        }
+        const items = {};
+        if (property.type == "object") {
+            items.properties = property.properties;
+            items.required = property.required;
+            delete property.properties;
+            delete property.required;
+        }
+        if (this.typeRef) {
+            items.$ref = property.$ref;
+            delete property.$ref;
+        } else {
+            items.type = property.type;
+            delete property.type;
+        }
+        property.type = "array";
+        property.items = items;
+        return property.items;
     }
 }
