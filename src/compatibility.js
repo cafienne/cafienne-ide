08f9b517cb9bde4d73536d83c7a1051e7644ccc9
Thijs Petter <thijspetter@gmail.com>
2024-05-24 12:04:16 +0200

    f

--- src/compatibility.js
+++ src/compatibility.js
@@ -1,6 +1,13 @@
+import CreateNewModelDialog from './ide/createnewmodeldialog';
+import DragData, { CaseFileItemDragData } from './ide/dragdata';
+import Dialog from './ide/editors/dialog';
 import MovableEditor from './ide/editors/movableeditor';
 import StandardForm from './ide/editors/standardform';
 import IDE from './ide/ide';
+import ModelListPanel from './ide/modellistpanel';
+import RepositoryBrowser from './ide/repositorybrowser';
+import Settings from './ide/settings/settings';
+import SettingsStorage from './ide/settings/settingsstorage';
 import CodeMirrorConfig from './util/codemirrorconfig';
 import Followup, { andThen, onFail } from './util/promise/followup';
 import FollowupList from './util/promise/followuplist';
@@ -20,6 +27,14 @@ const pointers = [
     onFail,
     MovableEditor,
     StandardForm,
+    Dialog,
+    CreateNewModelDialog,
+    DragData,
+    CaseFileItemDragData,
+    Settings,
+    SettingsStorage,
+    ModelListPanel,
+    RepositoryBrowser,
 ]
 
 export default class Compatibility {
