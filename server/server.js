39439ae237c4801da92fb6a61382f1ba5b8b47e3
jsrosman <jsrosman@gmail.com>
2024-06-21 10:34:34 +0200

    "jointjs": "~3.1.1"

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/node_modules', express.static(path.join(__dirname, '/../../node_modules')));
+
 // Do not add static content when running in a docker container.
 // The docker container serves static content via nginx
 if (app.get('env') !== 'docker') {
