0e2fee474c37f528bcfd33cf864bf3f0a4c4f330
jsrosman <jsrosman@gmail.com>
2024-07-01 15:43:19 +0200

    Port js -> canvaselement.ts and use import jointjs

---
+++ src/ide/modeleditor/case/elements/canvaselement.ts
@@ -0,0 +1,43 @@
+import { dia } from "jointjs";
+import CaseView from "./caseview";
+import CMMNElementView from "./cmmnelementview";
+
+export default class CanvasElement {
+    public case: CaseView;
+    private __jointElement?: dia.Element;
+
+    constructor(cs: CaseView) {
+        this.case = cs;
+    }
+
+    set xyz_joint(jointElement: dia.Element) {
+        this.__jointElement = jointElement;
+        (<any>jointElement).xyz_cmmn = this;
+    }
+
+    get xyz_joint() {
+        if (this.__jointElement) {
+            return this.__jointElement;
+        } else {
+            throw new Error('Too early')
+        }
+    }
+
+    /**
+     * Hook invoked after the element has moved.
+     * @param newParent - optional new parent 
+     */
+    moved(x: number, y: number, newParent: CMMNElementView) {        
+    }
+
+    /**
+     * Hook invoked upon mouseEnter
+     */
+    mouseEnter() {
+    }
+
+    /**
+     * Hook invoked upon mouseLeave
+     */
+    mouseLeave() {}
+}
