7967e0a76ccb33d3cb7d7943e35b31d20e56c7c5
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2024-12-12 10:59:09 +0100

    connected testcase harness with typescript client lib

--- src/testharness/index.ts
+++ src/testharness/index.ts
@@ -1,3 +1,18 @@
-import { Runner } from "./runner/runner";
+'use strict';
 
-export { Runner };
+import Repository from "@repository/repository";
+import FileSystemDefinitionStorage from "@repository/storage/filesytemdefinitionstorage";
+import Runner from "./runner/runner";
+
+// Main function
+(async () => {
+    try {
+        const args = process.argv.slice(2); // Get command-line arguments excluding the first two elements
+        const repository = new Repository(new FileSystemDefinitionStorage('./repository'));
+        await repository.listModels();
+        const runnerInstance = new Runner(repository);
+        await runnerInstance.runTests(args[0]);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})();
