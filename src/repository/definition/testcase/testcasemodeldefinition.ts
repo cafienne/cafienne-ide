423f517228961529c7ebead4f55c37738da0a3ba
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-01-08 10:19:44 +0100

    Final fixture execution

--- src/repository/definition/testcase/testcasemodeldefinition.ts
+++ src/repository/definition/testcase/testcasemodeldefinition.ts
@@ -3,16 +3,15 @@ import TestcaseFile from "@repository/serverfile/testcasefile";
 import FixtureDefinition from "./testfixturedefintion";
 
 export default class TestcaseModelDefinition extends ModelDefinition {
-    fixture?: FixtureDefinition;
+    fixture: FixtureDefinition;
 
     /**
      * Imports an XML element and parses it into a in-memory definition structure.
      */
     constructor(public file: TestcaseFile) {
         super(file);
-        this.fixture = this.parseElement('fixture', FixtureDefinition);
+        this.fixture = this.parseElement('fixture', FixtureDefinition) ?? (() => { throw new Error("Fixture not found"); })();
     }
-
 
     toXML() {
         const xmlDocument = super.exportModel('testcase', 'fixture');
--- src/testharness/index.ts
+++ src/testharness/index.ts
@@ -3,16 +3,33 @@
 import Repository from "@repository/repository";
 import FileSystemDefinitionStorage from "@repository/storage/filesytemdefinitionstorage";
 import Runner from "./runner/runner";
+import TestcaseInstance from "./runner/testcaseinstance";
 
 // Main function
 (async () => {
     try {
         const args = process.argv.slice(2); // Get command-line arguments excluding the first two elements
+        console.group(`Testharness ${args}`);
         const repository = new Repository(new FileSystemDefinitionStorage('./repository'));
         await repository.listModels();
         const runnerInstance = new Runner(repository);
-        await runnerInstance.runTests(args[0]);
+        const results = await runnerInstance.runTests(args[0]);
+
+        printResults(results);
     } catch (error) {
         console.error('Error:', error);
     }
+    finally {
+        console.groupEnd();
+    }
 })();
+
+function printResults(results: TestcaseInstance[]) {
+    results.forEach(result => {
+        console.group(`Testcase: ${result.testcase.name}: ${result.steps.filter(step => step.status === "failed").length > 0 ? "Failed" : "Passed"}`);
+        result.steps.forEach(step => {
+            console.log(`Step: ${step.name}: ${step.status} ${step.description}`);
+        });
+        console.groupEnd();
+    });
+}
