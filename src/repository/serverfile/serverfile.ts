casefabric/case-designer

Enabled file(local) and remote storage for repository

src/repository/serverfile/serverfile.ts
@@ -1,4 +1,3 @@
-import $ajax, { AjaxError } from "@util/ajax";
 import Util from "@util/util";
 import XML from "@util/xml";
 import ModelDefinition from "../definition/modeldefinition";
@@ -154,27 +153,7 @@ export default class ServerFile<M extends ModelDefinition> {
             return Promise.resolve(this);
         }
 
-        const url = '/repository/load/' + this.fileName;
-        const type = 'get';
-        console.log('Loading ' + url);
-
-        // Simple method for easy checking whether the functionality is still working ...
-        // this.usage();
-
-        const response = await $ajax({ type, url }).catch((error: AjaxError) => {
-            if (error.xhr && error.xhr.status === 404) {
-                throw `File "${this.fileName}" cannot be found in the repository. Perhaps it has been deleted`;
-            } else {
-                throw error;
-            }
-        });
-        if (response.xhr.responseText === '') {
-            const msg = this.fileName + ' does not exist or is an empty file in the repository';
-            console.warn(msg);
-            // we could reject?
-        }
-        console.log(`Fetched ${this.fileName}, calling parse with data `, response.data);
-        this.source = response.data;
+        this.source = await this.repository.definitionStorage.load(this.fileName);
         this.parse();
         console.log(`Parse ${this.fileName} is done`);
         return this;
@@ -247,17 +226,11 @@ export default class ServerFile<M extends ModelDefinition> {
      * Uploads the XML content to the server, and invokes the callback after it.
      * Uploading to server gives also a new file list back, which we use to update the repository contents.
      */
-    async save() {
-        const xmlString = XML.prettyPrint(this.source);
-        const url = '/repository/save/' + this.fileName;
-        const type = 'post';
-        console.groupCollapsed('Saving ' + this);
-        const response = await $ajax({ url, data: xmlString, type, headers: { 'content-type': 'application/xml' } }).catch(() => {
-            console.groupEnd();
-            throw 'We could not save your work due to an error in the server. Please refresh the browser and make sure the server is up and running';
-        });
+    async save(): Promise<void> {
+        console.groupCollapsed('Saving ' + this.fileName);
+        const newMetadata = await this.repository.definitionStorage.saveFile(this.fileName, this.source);
         this.hasBeenSavedJustNow = true;
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(newMetadata);
         this.hasBeenSavedJustNow = false;
         // Also print a timestampe of the new last modified information
         const lmDate = new Date(this.lastModified);
@@ -265,7 +238,6 @@ export default class ServerFile<M extends ModelDefinition> {
         const millis = ('000' + lmDate.getMilliseconds()).substr(-3);
         console.log('Uploaded ' + this + ' at ' + HHmmss + ':' + millis);
         console.groupEnd();
-        return response;
     }
 
     protected isRenaming = false;
@@ -295,16 +267,10 @@ export default class ServerFile<M extends ModelDefinition> {
             }
         }
 
-        console.groupCollapsed(`Renaming '${oldFileName}' to '${newFileName}'`);
-        const url = '/repository/rename/' + oldFileName + '?newName=' + newFileName;
-        const type = 'put';
-        const xmlString = XML.prettyPrint(this.definition?.toXMLString());
-
-        console.log(`Sending server request to rename '${oldFileName}' to '${newFileName}'`);
-        const response = await $ajax({ url, type, data: xmlString, headers: { 'content-type': 'application/xml' } }).catch((error: AjaxError) => { throw new Error('We could not rename the file: ' + error.message) });
+        const newMetadata = await this.repository.definitionStorage.renameFile(oldFileName, newFileName, this.definition?.toXML());
         this.hasBeenSavedJustNow = true;
         this.fileName = newFileName;
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(newMetadata);
         this.hasBeenSavedJustNow = false;
         const filesToBeSaved = this.usage;
         console.log(`Updating ${filesToBeSaved.length} files that have references to ${oldFileName}`)
@@ -321,11 +287,9 @@ export default class ServerFile<M extends ModelDefinition> {
      * Delete the file
      */
     async delete() {
-        const url = '/repository/delete/' + this.fileName;
-        const type = 'delete';
-        const response = await $ajax({ url, type });
+        const metadata = await this.repository.definitionStorage.deleteFile(this.fileName);
         this.repository.removeFile(this);
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(metadata);
         console.log('Deleted ' + this.fileName);
     }
 

src/repository/storage/definitionstorage.ts
@@ -1,0 +1,10 @@
+import Metadata from "@repository/serverfile/metadata";
+
+
+export default interface DefinitionStorage {
+    renameFile(fileName: any, newFileName: string, updatedContent: any): Promise<Metadata[]>;
+    saveFile(fileName: any, source: any): Promise<Metadata[]>;
+    deleteFile(fileName: any): Promise<Metadata[]>;
+    load(fileName: any): Promise<any>;
+    readAll(): Promise<Metadata[]>;
+}

src/repository/storage/filesytemdefinitionstorage.ts
@@ -1,0 +1,73 @@
+import Metadata from '@repository/serverfile/metadata';
+import DefinitionStorage from './definitionstorage';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function isKnownExtension(extension: string): boolean {
+    return ['.case', '.process', '.humantask', '.dimensions', '.cfid', '.type', '.xml'].indexOf(extension) >= 0;
+}
+
+export default class FileSystemDefinitionStorage implements DefinitionStorage {
+    constructor(private readonly definitionFolder: string) {
+        this.initialize();
+    }
+
+    private async initialize(): Promise<void> {
+        if (!fs.existsSync(this.definitionFolder)) {
+            await fs.promises.mkdir(this.definitionFolder, { recursive: true });
+        }
+    }
+
+    async renameFile(fileName: any, newFileName: string, updatedContent: any): Promise<Metadata[]> {
+        const oldPath = path.join(this.definitionFolder, fileName);
+        const newPath = path.join(this.definitionFolder, newFileName);
+
+        await fs.promises.rename(oldPath, newPath);
+        await fs.promises.writeFile(newPath, updatedContent, { encoding: 'utf8' });
+
+        return await this.fetchMetadata();
+    }
+
+    async saveFile(fileName: any, source: any): Promise<Metadata[]> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        await fs.promises.writeFile(fileName, source, { encoding: 'utf8' });
+        return await this.fetchMetadata();
+    }
+
+    async deleteFile(fileName: any): Promise<Metadata[]> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        await fs.promises.rm(filePath);
+        return await this.fetchMetadata();
+    }
+
+    async load(fileName: any): Promise<any> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        const data = await fs.promises.readFile(fileName, { encoding: 'utf8' });
+        return data;
+    }
+
+    async readAll(): Promise<Metadata[]> {
+        const metadata = await this.fetchMetadata();
+        await Promise.all(metadata.map(async (m) => (m.serverContent = await this.load(m.fileName))));
+        return metadata;
+    }
+
+    private async fetchMetadata(): Promise<Metadata[]> {
+        const files = await fs.promises.readdir(this.definitionFolder, { recursive: true });
+        return await Promise.all(
+            files
+                .filter((file) => isKnownExtension(path.extname(file)))
+                .map(async (file) => {
+                    const filePath = path.join(this.definitionFolder, file);
+                    const stats = await fs.promises.stat(filePath);
+                    return new Metadata({
+                        fileName: file,
+                        lastModified: stats.mtime,
+                        error: null,
+                        type: path.extname(file).substring(1),
+                        serverContent: null,
+                    });
+                }),
+        );
+    }
+}

src/repository/storage/remotedefinitionstorage.ts
@@ -1,0 +1,70 @@
+import $ajax, { $get, AjaxError } from '@util/ajax';
+import DefinitionStorage from './definitionstorage';
+import Metadata from '@repository/serverfile/metadata';
+import XML from '@util/xml';
+
+export default class RemoteDefinitionStorage implements DefinitionStorage {
+    repositoryUrl: string;
+    constructor(baseUrl: string) {
+        this.repositoryUrl = `${baseUrl}/repository`;
+    }
+
+    async renameFile(oldFileName: string, newFileName: string, updatedContent: any): Promise<Metadata[]> {
+        console.groupCollapsed(`Renaming '${oldFileName}' to '${newFileName}'`);
+        const url = `${this.repositoryUrl}/rename/${oldFileName}?newName=${newFileName}`;
+        const type = 'put';
+        const content = XML.prettyPrint(updatedContent);
+
+        console.log(`Sending server request to rename '${oldFileName}' to '${newFileName}'`);
+        const response = await $ajax({ url, type, data: content, headers: { 'content-type': 'application/xml' } })
+            .catch((error: AjaxError) => { throw new Error('We could not rename the file: ' + error.message) });
+
+        return response.data;
+    }
+    async saveFile(fileName: any, source: any): Promise<Metadata[]> {
+        const content = XML.prettyPrint(source);
+        const url = `${this.repositoryUrl}/save/${fileName}`;
+        const type = 'post';
+        const response = await $ajax({ url, data: content, type, headers: { 'content-type': 'application/xml' } })
+            .catch(() => {
+                throw 'We could not save your work due to an error in the server. Please refresh the browser and make sure the server is up and running';
+            });
+
+        return response.data;
+    }
+    async deleteFile(fileName: any): Promise<Metadata[]> {
+        const url = `${this.repositoryUrl}/delete/${fileName}`;
+        const type = 'delete';
+        const response = await $ajax({ url, type });
+
+        return response.data;
+    }
+    async load(fileName: string) {
+        const url = `${this.repositoryUrl}/load/${fileName}`;
+        const type = 'get';
+        console.log('Loading ' + url);
+
+        const response = await $ajax({ type, url }).catch((error: AjaxError) => {
+            if (error.xhr && error.xhr.status === 404) {
+                throw `File "${fileName}" cannot be found in the repository. Perhaps it has been deleted`;
+            } else {
+                throw error;
+            }
+        });
+        if (response.xhr.responseText === '') {
+            const msg = fileName + ' does not exist or is an empty file in the repository';
+            console.warn(msg);
+            // we could reject?
+        }
+        console.log(`Fetched ${fileName}, calling parse with data `, response.data);
+        return response.data;
+    }
+    async readAll(): Promise<Metadata[]> {
+        const url = `${this.repositoryUrl}/list`;
+        return await $get(url)
+            .catch((error: AjaxError) => {
+                console.error(error); // Actually also other errors may occur, therefore also logging the stacktrace
+                throw 'Could not fetch the list of models: ' + error.message
+            });
+    }
+}

cafienne/cafienne-ide

src/repository/serverfile/serverfile.ts
@@ -1,4 +1,3 @@
-import $ajax, { AjaxError } from "@util/ajax";
 import Util from "@util/util";
 import XML from "@util/xml";
 import ModelDefinition from "../definition/modeldefinition";
@@ -155,27 +154,7 @@ export default class ServerFile<M extends ModelDefinition> {
             return Promise.resolve(this);
         }
 
-        const url = '/repository/load/' + this.fileName;
-        const type = 'get';
-        console.log('Loading ' + url);
-
-        // Simple method for easy checking whether the functionality is still working ...
-        // this.usage();
-
-        const response = await $ajax({ type, url }).catch((error: AjaxError) => {
-            if (error.xhr && error.xhr.status === 404) {
-                throw `File "${this.fileName}" cannot be found in the repository. Perhaps it has been deleted`;
-            } else {
-                throw error;
-            }
-        });
-        if (response.xhr.responseText === '') {
-            const msg = this.fileName + ' does not exist or is an empty file in the repository';
-            console.warn(msg);
-            // we could reject?
-        }
-        console.log(`Fetched ${this.fileName}, calling parse with data `, response.data);
-        this.source = response.data;
+        this.source = await this.repository.definitionStorage.loadFile(this.fileName);
         this.parse();
         console.log(`Parse ${this.fileName} is done`);
         return this;
@@ -254,17 +233,11 @@ export default class ServerFile<M extends ModelDefinition> {
      * Uploads the XML content to the server, and invokes the callback after it.
      * Uploading to server gives also a new file list back, which we use to update the repository contents.
      */
-    async save() {
-        const xmlString = XML.prettyPrint(this.source);
-        const url = '/repository/save/' + this.fileName;
-        const type = 'post';
-        console.groupCollapsed('Saving ' + this);
-        const response = await $ajax({ url, data: xmlString, type, headers: { 'content-type': 'application/xml' } }).catch(() => {
-            console.groupEnd();
-            throw 'We could not save your work due to an error in the server. Please refresh the browser and make sure the server is up and running';
-        });
+    async save(): Promise<void> {
+        console.groupCollapsed('Saving ' + this.fileName);
+        const newMetadata = await this.repository.definitionStorage.saveFile(this.fileName, this.source);
         this.hasBeenSavedJustNow = true;
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(newMetadata);
         this.hasBeenSavedJustNow = false;
         // Also print a timestampe of the new last modified information
         const lmDate = new Date(this.lastModified);
@@ -272,7 +245,6 @@ export default class ServerFile<M extends ModelDefinition> {
         const millis = ('000' + lmDate.getMilliseconds()).substr(-3);
         console.log('Uploaded ' + this + ' at ' + HHmmss + ':' + millis);
         console.groupEnd();
-        return response;
     }
 
     protected isRenaming = false;
@@ -302,16 +274,10 @@ export default class ServerFile<M extends ModelDefinition> {
             }
         }
 
-        console.groupCollapsed(`Renaming '${oldFileName}' to '${newFileName}'`);
-        const url = '/repository/rename/' + oldFileName + '?newName=' + newFileName;
-        const type = 'put';
-        const xmlString = XML.prettyPrint(this.definition?.toXMLString());
-
-        console.log(`Sending server request to rename '${oldFileName}' to '${newFileName}'`);
-        const response = await $ajax({ url, type, data: xmlString, headers: { 'content-type': 'application/xml' } }).catch((error: AjaxError) => { throw new Error('We could not rename the file: ' + error.message) });
+        const newMetadata = await this.repository.definitionStorage.renameFile(oldFileName, newFileName, this.definition?.toXML());
         this.hasBeenSavedJustNow = true;
         this.fileName = newFileName;
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(newMetadata);
         this.hasBeenSavedJustNow = false;
         const filesToBeSaved = this.usage;
         console.log(`Updating ${filesToBeSaved.length} files that have references to ${oldFileName}`)
@@ -328,11 +294,9 @@ export default class ServerFile<M extends ModelDefinition> {
      * Delete the file
      */
     async delete() {
-        const url = '/repository/delete/' + this.fileName;
-        const type = 'delete';
-        const response = await $ajax({ url, type });
+        const metadata = await this.repository.definitionStorage.deleteFile(this.fileName);
         this.repository.removeFile(this);
-        await this.repository.updateMetadata(response.data);
+        await this.repository.updateMetadata(metadata);
         console.log('Deleted ' + this.fileName);
     }
 

src/repository/storage/definitionstorage.ts
@@ -1,0 +1,9 @@
+import Metadata from "@repository/serverfile/metadata";
+
+export default interface DefinitionStorage {
+    renameFile(fileName: any, newFileName: string, updatedContent: any): Promise<Metadata[]>;
+    saveFile(fileName: any, source: any): Promise<Metadata[]>;
+    deleteFile(fileName: any): Promise<Metadata[]>;
+    loadFile(fileName: any): Promise<any>;
+    loadAllFiles(): Promise<Metadata[]>;
+}

src/repository/storage/filesytemdefinitionstorage.ts
@@ -1,0 +1,73 @@
+import Metadata from '@repository/serverfile/metadata';
+import DefinitionStorage from './definitionstorage';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function isKnownExtension(extension: string): boolean {
+    return ['.case', '.process', '.humantask', '.dimensions', '.cfid', '.type', '.xml'].indexOf(extension) >= 0;
+}
+
+export default class FileSystemDefinitionStorage implements DefinitionStorage {
+    constructor(private readonly definitionFolder: string) {
+        this.initialize();
+    }
+
+    private async initialize(): Promise<void> {
+        if (!fs.existsSync(this.definitionFolder)) {
+            await fs.promises.mkdir(this.definitionFolder, { recursive: true });
+        }
+    }
+
+    async renameFile(fileName: any, newFileName: string, updatedContent: any): Promise<Metadata[]> {
+        const oldPath = path.join(this.definitionFolder, fileName);
+        const newPath = path.join(this.definitionFolder, newFileName);
+
+        await fs.promises.rename(oldPath, newPath);
+        await fs.promises.writeFile(newPath, updatedContent, { encoding: 'utf8' });
+
+        return await this.fetchMetadata();
+    }
+
+    async saveFile(fileName: any, source: any): Promise<Metadata[]> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        await fs.promises.writeFile(fileName, source, { encoding: 'utf8' });
+        return await this.fetchMetadata();
+    }
+
+    async deleteFile(fileName: any): Promise<Metadata[]> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        await fs.promises.rm(filePath);
+        return await this.fetchMetadata();
+    }
+
+    async loadFile(fileName: any): Promise<any> {
+        const filePath = path.join(this.definitionFolder, fileName);
+        const data = await fs.promises.readFile(filePath, { encoding: 'utf8' });
+        return data;
+    }
+
+    async loadAllFiles(): Promise<Metadata[]> {
+        const metadata = await this.fetchMetadata();
+        await Promise.all(metadata.map(async (m) => (m.content = await this.loadFile(m.fileName))));
+        return metadata;
+    }
+
+    private async fetchMetadata(): Promise<Metadata[]> {
+        const files = await fs.promises.readdir(this.definitionFolder, { recursive: true });
+        return await Promise.all(
+            files
+                .filter((file) => isKnownExtension(path.extname(file)))
+                .map(async (file) => {
+                    const filePath = path.join(this.definitionFolder, file);
+                    const stats = await fs.promises.stat(filePath);
+                    return new Metadata({
+                        fileName: file,
+                        lastModified: stats.mtime,
+                        error: null,
+                        type: path.extname(file).substring(1),
+                        serverContent: null,
+                    });
+                }),
+        );
+    }
+}

src/repository/storage/remotedefinitionstorage.ts
@@ -1,0 +1,69 @@
+import $ajax, { $get, AjaxError } from '@util/ajax';
+import DefinitionStorage from './definitionstorage';
+import Metadata from '@repository/serverfile/metadata';
+import XML from '@util/xml';
+
+export default class RemoteDefinitionStorage implements DefinitionStorage {
+    repositoryUrl: string;
+    constructor(baseUrl: string) {
+        this.repositoryUrl = `${baseUrl}/repository`;
+    }
+
+    async renameFile(oldFileName: string, newFileName: string, updatedContent: any): Promise<Metadata[]> {
+        console.groupCollapsed(`Renaming '${oldFileName}' to '${newFileName}'`);
+        const url = `${this.repositoryUrl}/rename/${oldFileName}?newName=${newFileName}`;
+        const type = 'put';
+        const content = XML.prettyPrint(updatedContent);
+
+        console.log(`Sending server request to rename '${oldFileName}' to '${newFileName}'`);
+        const response = await $ajax({ url, type, data: content, headers: { 'content-type': 'application/xml' } })
+            .catch((error: AjaxError) => { throw new Error('We could not rename the file: ' + error.message) });
+
+        return response.data;
+    }
+    async saveFile(fileName: any, source: any): Promise<Metadata[]> {
+        const content = XML.prettyPrint(source);
+        const url = `${this.repositoryUrl}/save/${fileName}`;
+        const type = 'post';
+        const response = await $ajax({ url, data: content, type, headers: { 'content-type': 'application/xml' } })
+            .catch(() => {
+                throw 'We could not save your work due to an error in the server. Please refresh the browser and make sure the server is up and running';
+            });
+
+        return response.data;
+    }
+    async deleteFile(fileName: any): Promise<Metadata[]> {
+        const url = `${this.repositoryUrl}/delete/${fileName}`;
+        const type = 'delete';
+        const response = await $ajax({ url, type });
+
+        return response.data;
+    }
+    async loadFile(fileName: string) {
+        const url = `${this.repositoryUrl}/load/${fileName}`;
+        const type = 'get';
+        console.log('Loading ' + url);
+
+        const response = await $ajax({ type, url }).catch((error: AjaxError) => {
+            if (error.xhr && error.xhr.status === 404) {
+                throw `File "${fileName}" cannot be found in the repository. Perhaps it has been deleted`;
+            } else {
+                throw error;
+            }
+        });
+        if (response.xhr.responseText === '') {
+            const msg = fileName + ' does not exist or is an empty file in the repository';
+            console.warn(msg);
+            // we could reject?
+        }
+        console.log(`Fetched ${fileName}, calling parse with data `, response.data);
+        return response.data;
+    }
+    async loadAllFiles(): Promise<Metadata[]> {
+        const url = `${this.repositoryUrl}/list`;
+        return await $get(url)
+            .catch((error: AjaxError) => {
+                throw 'Could not fetch the list of models: ' + error.message
+            });
+    }
+}