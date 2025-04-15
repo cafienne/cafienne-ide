21d0c759b8aa76926d3e7d6c11aea8048e58606d
Thijs Petter <thijspetter@gmail.com>
2025-04-15 13:02:51 +0200

    Update ifpartdefinition.ts

--- src/repository/definition/cmmn/sentry/ifpartdefinition.ts
+++ src/repository/definition/cmmn/sentry/ifpartdefinition.ts
@@ -1,8 +1,13 @@
 import { Element } from "../../../../util/xml";
+import Validator from "../../../validate/validator";
 import ConstraintDefinition from "../caseplan/constraintdefinition";
 
 export default class IfPartDefinition extends ConstraintDefinition {
     createExportNode(parentNode: Element) {
         super.createExportNode(parentNode, 'ifPart');
+    }
+
+    validate(validator: Validator): void {
+        // Check that we're not empty if defined...
     }
 }
