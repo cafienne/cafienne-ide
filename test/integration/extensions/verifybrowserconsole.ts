2b70403d7018d5a036c2d0567e0c14edf3a1c3b0
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-07-25 15:42:31 +0200

    Fix incomplete delete of team from teameditor

--- test/integration/extensions/verifybrowserconsole.ts
+++ test/integration/extensions/verifybrowserconsole.ts
@@ -6,8 +6,6 @@ type logEvent = {
 
 const ignoreEvents = [
     /: no source content to parse/, // filter out those warnings due to parallel execution;
-    /CaseTeam: Did not receive a file for [^\s]*.caseteam/, // warning due to problem in caseteam editor with remove of team
-    /Still using [^\s]*.caseteam \?\?\?  Better not, since it no longer exists in the server .../, // same as above
 ]
 
 export const mochaHooks = {
